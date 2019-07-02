0d69aca67ac0caf6f71f4ccf033741af6d9a65a3
Александр Виноградов <a.vinogradov@softpro.com>
2019-07-02 20:14:01 +0300

    change metrics nuget ref

--- src/publish-nuget.bat
+++ src/publish-nuget.bat
@@ -1,6 +1,6 @@
-dotnet nuget push Sportradar.OddsFeed.SDK\bin\Release\Sportradar.OddsFeed.SDK-SoftPro-Fork.1.22.1.2.nupkg -k #APIKEY# -s https://api.nuget.org/v3/index.json
-dotnet nuget push Sportradar.OddsFeed.SDK.API\bin\Release\Sportradar.OddsFeed.SDK.API-SoftPro-Fork.1.0.0.nupkg -k #APIKEY# -s https://api.nuget.org/v3/index.json
-dotnet nuget push Sportradar.OddsFeed.SDK.Common\bin\Release\Sportradar.OddsFeed.SDK.Common-SoftPro-Fork.1.0.0.nupkg -k #APIKEY# -s https://api.nuget.org/v3/index.json
-dotnet nuget push Sportradar.OddsFeed.SDK.Entities\bin\Release\Sportradar.OddsFeed.SDK.Entities-SoftPro-Fork.1.0.0.nupkg -k #APIKEY# -s https://api.nuget.org/v3/index.json
-dotnet nuget push Sportradar.OddsFeed.SDK.Entities.REST\bin\Release\Sportradar.OddsFeed.SDK.Entities.REST-SoftPro-Fork.1.0.0.nupkg -k #APIKEY# -s https://api.nuget.org/v3/index.json
-dotnet nuget push Sportradar.OddsFeed.SDK.Messages\bin\Release\Sportradar.OddsFeed.SDK.Messages-SoftPro-Fork.1.0.0.nupkg -k #APIKEY# -s https://api.nuget.org/v3/index.json
+dotnet nuget push Sportradar.OddsFeed.SDK\bin\Release\Sportradar.OddsFeed.SDK-SoftPro-Fork.1.22.1.3.nupkg -k #ApiKey# -s https://api.nuget.org/v3/index.json
+dotnet nuget push Sportradar.OddsFeed.SDK.API\bin\Release\Sportradar.OddsFeed.SDK.API-SoftPro-Fork.1.0.1.nupkg -k #ApiKey# -s https://api.nuget.org/v3/index.json
+dotnet nuget push Sportradar.OddsFeed.SDK.Common\bin\Release\Sportradar.OddsFeed.SDK.Common-SoftPro-Fork.1.0.1.nupkg -k #ApiKey# -s https://api.nuget.org/v3/index.json
+dotnet nuget push Sportradar.OddsFeed.SDK.Entities\bin\Release\Sportradar.OddsFeed.SDK.Entities-SoftPro-Fork.1.0.1.nupkg -k #ApiKey# -s https://api.nuget.org/v3/index.json
+dotnet nuget push Sportradar.OddsFeed.SDK.Entities.REST\bin\Release\Sportradar.OddsFeed.SDK.Entities.REST-SoftPro-Fork.1.0.1.nupkg -k #ApiKey# -s https://api.nuget.org/v3/index.json
+dotnet nuget push Sportradar.OddsFeed.SDK.Messages\bin\Release\Sportradar.OddsFeed.SDK.Messages-SoftPro-Fork.1.0.1.nupkg -k #ApiKey# -s https://api.nuget.org/v3/index.json
